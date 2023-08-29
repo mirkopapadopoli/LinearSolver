18523268f626181698d6d04524954ef14bf7ccce
Mirko <papadopoli.mirko@gmail.com>
2023-08-29 19:09:04 +0200

    some update

--- Python/progetto1_mac.py
+++ Python/progetto1_mac.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.linalg as sc
 import os, csv, platform, psutil
 
 from scipy.sparse.linalg import spsolve
@@ -13,7 +12,6 @@ path_matrix = "/Users/mirkopapadopoli/Code/LinearSolver/Matrix/"
 header_csv = ['Software', 'Sistema', 'Matrice', 'Dimensione', 'Errore Relativo', 'Tempo Esecuzione (sec)', 'Memoria (MB)']
 
 def calculateFunction(matrixList):
-
     # Read Matrix and convert to Compressed Sparse Row matrix
     M = mmread(path_matrix + matrixList).tocsc()
     M = csc_matrix(M)
@@ -21,7 +19,6 @@ def calculateFunction(matrixList):
     #print("Matrix: ", matrixList)
     dimension = M.shape
     #print("Dimension: ", M.shape)
-    
     # Identify the matrix
     xe = np.ones(M.shape[0])
 
@@ -29,41 +26,27 @@ def calculateFunction(matrixList):
     b = M * xe
     print("Eseguo " + matrixList + " ...")
     # Start timer
-    #startTime = time.process_time()
     startTime = datetime.now()
-    #print(startTime)
     # Start Memory
     startMemory = psutil.swap_memory()[1] / (1024 ** 2)
-
-    # Decomposition Cholesky
-    #M = sc.cholesky(M.todense(), lower=True)
-
     # Calculate the solution of the linear system
     x = spsolve(M, b)#, use_umfpack=True)
-
     #End Memory
     endMemory = max(memory_usage((spsolve, (M, b)))) + (psutil.swap_memory()[1] / (1024 ** 2))
-
     # Stop timer
-    # endTime = time.process_time()
     endTime = datetime.now()
-    #print(endTime)
     # Calculate relative error
     rel_Error = np.linalg.norm(x - xe) / np.linalg.norm(xe)
-
     # Calculate execution time - memory usage
     exec_Time = (endTime - startTime)
     exec_Memory = (endMemory - startMemory)
 
     # Check relative error solution
     print("Relative error is: ", np.linalg.norm(x - xe) / np.linalg.norm(xe))
-
     # Check time execution
     print("Time execution of Cholesky: " + str((endTime - startTime)), " sec")
-
     # Check memory usage
     print("Memory used: " + str(endMemory - startMemory), " MB")
-
     # Check Operative System
     system = platform.system()
 
